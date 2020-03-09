83edc3bcfed260f11a0b2175c78973370462c958
Baker Mwanje <mwanjebakamgmail.com>
2020-03-09 10:17:06 +0100

    3rd

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'BakerMwanje',
+        short_name: 'BakerMwanje',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
